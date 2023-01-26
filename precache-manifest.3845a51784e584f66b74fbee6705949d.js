8e3317d2a2833d8a45a76ad846978ac0c05bedd7
redhotchel <75904433+redhotchel@users.noreply.github.com>
2023-01-26 13:07:50 +0500

    Add files via upload

---
+++ precache-manifest.3845a51784e584f66b74fbee6705949d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd81cbaef5a579a895bedd8b8543d68",
+    "url": "//redhotchel.github.io/social-site/index.html"
+  },
+  {
+    "revision": "4c64e20f30cddfa7b4ab",
+    "url": "//redhotchel.github.io/social-site/static/css/3.71f71379.chunk.css"
+  },
+  {
+    "revision": "77b63be69199769ea74d",
+    "url": "//redhotchel.github.io/social-site/static/css/4.9024c852.chunk.css"
+  },
+  {
+    "revision": "5b2a583163d9237e6c50",
+    "url": "//redhotchel.github.io/social-site/static/css/main.fa8c41f1.chunk.css"
+  },
+  {
+    "revision": "898d5cc75406e1509dbf",
+    "url": "//redhotchel.github.io/social-site/static/js/2.e0be956d.chunk.js"
+  },
+  {
+    "revision": "4c64e20f30cddfa7b4ab",
+    "url": "//redhotchel.github.io/social-site/static/js/3.223406bc.chunk.js"
+  },
+  {
+    "revision": "77b63be69199769ea74d",
+    "url": "//redhotchel.github.io/social-site/static/js/4.4cb46c02.chunk.js"
+  },
+  {
+    "revision": "5b2a583163d9237e6c50",
+    "url": "//redhotchel.github.io/social-site/static/js/main.bd7bd0d2.chunk.js"
+  },
+  {
+    "revision": "f83b6e29e05ca312d187",
+    "url": "//redhotchel.github.io/social-site/static/js/runtime~main.5c99cb51.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "//redhotchel.github.io/social-site/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "//redhotchel.github.io/social-site/static/media/user.8b1dbb1f.png"
+  }
+]);
